89035f61c7215690ca32e537d82c590b7f2f2175
Raman Varabets <roman.vorobets@gmail.com>
2018-10-25 21:55:39 +0800

    fix active stream detection, store stream type

--- src/lwt_gauge.ml
+++ src/lwt_gauge.ml
@@ -1,15 +1,46 @@
 
 let log = Log.from "lwt_gauge"
 
+type stream_type =
+  | From
+  | FromDirect
+  | Bounded
+  | Unbounded
+  | UnboundedWithRef
+  | OfList
+  | OfArray
+  | OfString
+  | Clone
+
+let string_of_stream_type = function
+  | From -> "from"
+  | FromDirect -> "from_direct"
+  | Bounded -> "create_bounded"
+  | Unbounded -> "create"
+  | UnboundedWithRef -> "create_with_reference"
+  | OfList -> "of_list"
+  | OfArray -> "of_array"
+  | OfString -> "of_string"
+  | Clone -> "clone"
+
 type stream = {
+  type_ : stream_type;
   name : string option;
+  is_active : unit -> bool;
 }
 
 type gauge = {
   mutable streams : stream Dllist.node_t option;
 }
 
-let gauge g s stream =
+let gauge g type_ ~name stream =
+  let is_active () =
+    not (Lwt_stream.is_closed stream) ||
+    match Lwt.state (Lwt_stream.is_empty stream) with
+    | Return is_empty -> not is_empty
+    | Sleep | Fail _ -> assert false (* if is_closed, is_empty is guaranteed not to block *)
+  in
+  let s = { type_; name; is_active; } in
   let handle =
     match g.streams with
     | Some streams -> Dllist.prepend streams s
@@ -33,10 +64,16 @@ let end_gauge { streams; } =
   match streams with
   | None -> ()
   | Some streams ->
-  let streams = Dllist.to_list streams in
+  let streams =
+    Dllist.to_list streams |>
+    List.filter (fun { is_active; _ } -> is_active ())
+  in
+  match streams with
+  | [] -> ()
+  | _ ->
   log #warn "%d streams are still active:" (List.length streams);
-  List.iteri begin fun i { name; } ->
-    log #warn "#%d %s is still active" i (Option.default "<unnamed>" name)
+  List.iteri begin fun i { name; type_; _ } ->
+    log #warn "%4d) Lwt_stream.%s ~name:%S" (i + 1) (string_of_stream_type type_) (Option.default "<unnamed>" name)
   end streams
 
 module Lwt_stream = struct
@@ -48,60 +85,57 @@ module Lwt_stream = struct
   let from ?name f =
     match Lwt.get tls with
     | None -> from f
-    | Some g -> gauge g { name; } (from f)
+    | Some g -> gauge g From ~name (from f)
 
   let from_direct ?name f =
     match Lwt.get tls with
     | None -> from_direct f
-    | Some g -> gauge g { name; } (from_direct f)
+    | Some g -> gauge g FromDirect ~name (from_direct f)
 
   let create ?name () =
     match Lwt.get tls with
     | None -> create ()
     | Some g ->
     let (s, f) = create () in
-    gauge g { name; } s, f
+    gauge g Unbounded ~name s, f
 
   let create_with_reference ?name () =
     match Lwt.get tls with
     | None -> create_with_reference ()
     | Some g ->
     let (s, f, r) = create_with_reference () in
-    gauge g { name; } s, f, r
+    gauge g Unbounded ~name s, f, r
 
   let create_bounded ?name n =
     match Lwt.get tls with
     | None -> create_bounded n
     | Some g ->
     let (s, p) = create_bounded n in
-    gauge g { name; } s, p
+    gauge g Bounded ~name s, p
 
   let of_list ?name l =
     match Lwt.get tls with
     | None -> of_list l
-    | Some g -> gauge g { name; } (of_list l)
+    | Some g -> gauge g OfList ~name (of_list l)
 
   let of_array ?name a =
     match Lwt.get tls with
     | None -> of_array a
-    | Some g -> gauge g { name; } (of_array a)
+    | Some g -> gauge g OfArray ~name (of_array a)
 
   let of_string ?name s =
     match Lwt.get tls with
     | None -> of_string s
-    | Some g -> gauge g { name; } (of_string s)
+    | Some g -> gauge g OfString ~name (of_string s)
 
   let clone ?name s =
     match Lwt.get tls with
     | None -> clone s
-    | Some g -> gauge g { name; } (clone s)
+    | Some g -> gauge g Clone ~name (clone s)
 
-  let with_gauge ~enable f =
-    match enable with
-    | false -> f
-    | true ->
+  let with_gauge f =
     let gauge = { streams = None; } in
     Lwt.with_value tls (Some gauge) @@ fun () ->
-    (f) [%lwt.finally end_gauge gauge; Lwt.return_unit; ]
+    (f ()) [%lwt.finally end_gauge gauge; Lwt.return_unit; ]
 
 end
