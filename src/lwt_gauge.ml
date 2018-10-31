5a873808dc7f6f4d8543b2bd139b23b5ff0536cc
Raman Varabets <roman.vorobets@gmail.com>
2018-10-31 20:12:33 +0800

    use global state instead of lwt tls

--- src/lwt_gauge.ml
+++ src/lwt_gauge.ml
@@ -57,11 +57,15 @@ module Gauge = struct
     controls : controls;
   >
 
+  let is_active : unit Lwt.key = Lwt.new_key ()
+
   type global_state = {
-    mutable streams : gauge Dllist.node_t option;
+    mutable gauges : gauge Dllist.node_t option;
   }
 
-  let tls : global_state Lwt.key = Lwt.new_key ()
+  let global_state = {
+    gauges = None;
+  }
 
   let make'' type_ ~name ?(count=0) ?size () =
     let props = { type_; name; count; size; } in
@@ -86,22 +90,22 @@ module Gauge = struct
     let controls = make'' type_ ~name ?count ?size () in
     make' controls stream
 
-  let attach g s stream =
+  let attach s stream =
     let handle =
-      match g.streams with
-      | Some streams -> Dllist.prepend streams s
+      match global_state.gauges with
+      | Some gauges -> Dllist.prepend gauges s
       | None ->
-      let streams = Dllist.create s in
-      g.streams <- Some streams;
-      streams
+      let gauges = Dllist.create s in
+      global_state.gauges <- Some gauges;
+      gauges
     in
     let remove () =
-      match g.streams with
+      match global_state.gauges with
       | None -> assert false
-      | Some streams when streams != handle -> Dllist.remove handle
+      | Some gauges when gauges != handle -> Dllist.remove handle
       | Some _ ->
       let next = Dllist.drop handle in
-      g.streams <- if next != handle then Some next else None
+      global_state.gauges <- if next != handle then Some next else None
     in
     Lwt_stream.from begin fun () ->
       match%lwt Lwt_stream.get stream with
@@ -109,18 +113,18 @@ module Gauge = struct
       | None -> remove (); Lwt.return_none
     end
 
-  let gauge g type_ ~name ?count ?size stream =
+  let gauge type_ ~name ?count ?size stream =
     let s = make type_ ~name ?count ?size stream in
-    attach g s stream
+    attach s stream
 
   let probe_all () =
-    match Lwt.get tls with
+    match Lwt.get is_active with
     | None -> []
-    | Some { streams; } ->
-    match streams with
+    | Some _ ->
+    match global_state.gauges with
     | None -> []
-    | Some streams ->
-    Dllist.to_list streams |>
+    | Some gauges ->
+    Dllist.to_list gauges |>
     List.map begin fun s ->
       let { type_; name; count; size; } = s #controls #props in
       let is_closed = s #is_closed in
@@ -148,9 +152,9 @@ module Lwt_stream = struct
   open Gauge
 
   let from ?name f =
-    match Lwt.get tls with
+    match Lwt.get is_active with
     | None -> from f
-    | Some g ->
+    | Some _ ->
     let controls = make'' From ~name () in
     let f () =
       match%lwt f () with
@@ -159,12 +163,12 @@ module Lwt_stream = struct
     in
     let s = from f in
     let s' = make' controls s in
-    attach g s' s
+    attach s' s
 
   let from_direct ?name f =
-    match Lwt.get tls with
+    match Lwt.get is_active with
     | None -> from_direct f
-    | Some g ->
+    | Some _ ->
     let controls = make'' FromDirect ~name () in
     let f () =
       match f () with
@@ -173,33 +177,33 @@ module Lwt_stream = struct
     in
     let s = from_direct f in
     let s' = make' controls s in
-    attach g s' s
+    attach s' s
 
   let create ?name () =
-    match Lwt.get tls with
+    match Lwt.get is_active with
     | None -> create ()
-    | Some g ->
+    | Some _ ->
     let (s, f) = create () in
     let s' = make Unbounded ~name s in
     let f = function Some _ as x -> s' #controls #incr; f x | None -> f None in
-    attach g s' s, f
+    attach s' s, f
 
   let create_with_reference ?name () =
-    match Lwt.get tls with
+    match Lwt.get is_active with
     | None -> create_with_reference ()
-    | Some g ->
+    | Some _ ->
     let (s, f, r) = create_with_reference () in
     let s' = make Unbounded ~name s in
     let f = function Some _ as x -> s' #controls #incr; f x | None -> f None in
-    attach g s' s, f, r
+    attach s' s, f, r
 
   let create_bounded ?name n =
-    match Lwt.get tls with
+    match Lwt.get is_active with
     | None -> create_bounded n
-    | Some g ->
+    | Some _ ->
     let (s, p) = create_bounded n in
     let s' = make Bounded ~name ~size:n s in
-    let s = attach g s' s in
+    let s = attach s' s in
     s, object
       method size = p #size
       method resize n = s' #controls #resize n; p #resize n
@@ -212,42 +216,41 @@ module Lwt_stream = struct
     end
 
   let of_list ?name l =
-    match Lwt.get tls with
+    match Lwt.get is_active with
     | None -> of_list l
-    | Some g ->
+    | Some _ ->
     let n = List.length l in
-    gauge g OfList ~name ~count:n ~size:n (of_list l)
+    gauge OfList ~name ~count:n ~size:n (of_list l)
 
   let of_array ?name a =
-    match Lwt.get tls with
+    match Lwt.get is_active with
     | None -> of_array a
-    | Some g ->
+    | Some _ ->
     let n = Array.length a in
-    gauge g OfArray ~name ~count:n ~size:n (of_array a)
+    gauge OfArray ~name ~count:n ~size:n (of_array a)
 
   let of_string ?name s =
-    match Lwt.get tls with
+    match Lwt.get is_active with
     | None -> of_string s
-    | Some g ->
+    | Some _ ->
     let n = String.length s in
-    gauge g OfString ~name ~count:n ~size:n (of_string s)
+    gauge OfString ~name ~count:n ~size:n (of_string s)
 
   let clone ?name s =
-    match Lwt.get tls with
+    match Lwt.get is_active with
     | None -> clone s
-    | Some g -> gauge g Clone ~name (clone s)
+    | Some _ -> gauge Clone ~name (clone s)
 
   let with_gauge f =
-    let gauge = { streams = None; } in
-    Lwt.with_value tls (Some gauge) @@ fun () ->
+    Lwt.with_value is_active (Some ()) @@ fun () ->
     let fin () =
       match probe_all () with
       | [] -> ()
-      | streams ->
-      log #warn "%d streams are still active:" (List.length streams);
+      | gauges ->
+      log #warn "%d streams are still active:" (List.length gauges);
       List.iteri begin fun i probe ->
         log #warn "%4d) %s" (i + 1) (show_probe probe)
-      end streams
+      end gauges
     in
     (f ()) [%lwt.finally fin (); Lwt.return_unit; ]
 
